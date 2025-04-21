fe094521e2eba8410c4cac1e78a2126cea455204
SantiM78 <95258911+SantiM78@users.noreply.github.com>
2025-04-20 19:13:44 -0500

    Estilo home

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import AppRoutes from './routes/AppRoutes';
+import './styles/App.scss'
+import './styles/home.scss'
+import Navbar from './components/Navbar';
+
 
 function App() {
   return <AppRoutes />;
